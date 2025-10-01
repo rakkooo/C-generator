1741107d1fef787db819e18fe5ba9edf33a188d9
rakko <99783550+rakkooo@users.noreply.github.com>
2025-10-01 21:50:56 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -236,7 +236,7 @@ export default function PFPGenerator() {
 
   const handleExternalLinkConfirm = () => {
     setShowExternalLinkConfirm(false)
-    window.open("https://forms.gle/QdjjXp6qWoeswrAS7", "_blank")
+    window.open("https://forms.gle/xeYKvifyXZuwsuwo6", "_blank")
   }
 
   return (
